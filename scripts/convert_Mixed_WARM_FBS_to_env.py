95d9441cee182994e5dcaee587dfe6eebd922e73
WesIngwersen <ingwersen.wesley@epa.gov>
2022-08-17 10:34:35 -0400

    Add script and result of script to create disagg env file from Mixed WARM FBS

--- scripts/convert_Mixed_WARM_FBS_to_env.py
+++ scripts/convert_Mixed_WARM_FBS_to_env.py
@@ -15,22 +15,28 @@ fbs_name = "Mixed_WARM_national_2018"
 env_name = "Mixed_WARM_national_2018_Env.csv"
 disagg_path = os.path.realpath('useeior/disaggspecs/')
 
-model_material_codes = ['F','C']  # from Materials reference file
+model_material_codes = ['F', 'C']  # from Materials reference file
 
+# Get flow by sector in fbs collapsed format
 fbsc = flowsa.collapse_FlowBySector(fbs_name)
 
+# Add satellite tables for each flow
 sat_mapping_file = 'data/mapping_warm_flowables_to_satellite_table.yml'
 with open(sat_mapping_file, 'r') as file:
     sat_mappings = yaml.safe_load(file)
 sat_map_df = pd.DataFrame(data={'Flowable':sat_mappings.keys(), 'SatelliteTable':sat_mappings.values()})
-
 fbsc = pd.merge(fbsc,sat_map_df,on='Flowable',how='left')
 
+#Change location from a FIPS code to US
+fbsc['Location'].replace({'00000':'US'}, inplace=True)
+
+#Aggregate flows for material sectors not in material_model_codes into an other set
 fbsc = agg_fbsc_by_material(fbsc, model_material_codes)
 
 env = convert_fbsc_to_disagg_env(fbsc)
 
-env.to_csv(disagg_path + env_name, index=False)
-logging.info(env_name + " written to " + disagg_path)
+env_file = os.path.join(disagg_path, env_name)
+env.to_csv(env_file, index=False)
+print(env_name + " written to " + disagg_path)
 
 

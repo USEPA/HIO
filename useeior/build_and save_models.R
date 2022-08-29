91810674d8f495796d22404c17f5e57422210a27
WesIngwersen <ingwersen.wesley@epa.gov>
2022-08-29 13:34:21 -0400

    Add logging to build_and_save models

--- useeior/build_and save_models.R
+++ useeior/build_and save_models.R
@@ -6,13 +6,14 @@
 library(devtools)
 devtools::load_all("../../useeior")
 
+
 modelmeta <- list("USEEIOv2.0.2-walrus"= c("USEEIOv2.0.2-walrus"),
                   "USEEIO-WARM"= c("USEEIO-WARM", "WARMv15"),
                   "USEEIO-Waste-Disagg"= c("USEEIO-Waste-Disagg","LandfillingDisaggregation","562OTHDisaggregation",
                                           "WasteCombustorsDisaggregation","MRFsDisaggregation"),
                   "USEEIO-REI"= c("USEEIO-REI","REI_WIO"),
                   "USEEIO-WIO-Disagg"= c("USEEIO-WIO-Disagg","DisaggWIOConcrete","LandfillingDisaggregation",
-                                         "562OTHDisaggregation","WasteCombustorsDisaggregation","MRFsDisaggregation")
+                                         "562OTHDisaggregation", "WasteCombustorsDisaggregation", "MRFsDisaggregation")
                   )
 
 for (name in names(modelmeta)) {

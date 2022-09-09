f35988f1fe83e4d2e75f85b29c38f93d38b63755
Ben Young <Ben.Young@erg.com>
2022-09-09 12:11:20 -0400

    collapse indicators for duplicate mappings for IPCC

--- lciafmt/store_mapped_IPCC_AR4100.py
+++ lciafmt/store_mapped_IPCC_AR4100.py
@@ -5,13 +5,15 @@ Writes method and associated metadata file to local cache
 """
 
 import lciafmt
-from lciafmt.util import store_method
+from lciafmt.util import store_method, collapse_indicators
 
 ipcc = lciafmt.get_method("IPCC")
 
 ipcc_AR4_100 = ipcc[ipcc["Indicator"]=="AR4-100"]
 
 ipcc_AR4_100 = lciafmt.map_flows(ipcc_AR4_100,"IPCC")
+
+ipcc_AR4_100 = collapse_indicators(ipcc_AR4_100)
 
 store_method(ipcc_AR4_100,lciafmt.Method.IPCC,name="IPCC_AR4-100")
 

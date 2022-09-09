c48966659f453a78c0a667de6aaeb0c72e979a24
WesIngwersen <ingwersen.wesley@epa.gov>
2022-09-09 10:44:49 -0400

    update to build and save reference models.. save names start with REF- and HIO-

--- useeior/build_and save_models.R
+++ useeior/build_and save_models.R
@@ -1,10 +1,13 @@
 library(devtools)
 devtools::load_all("../../useeior")
 library(logging)
-addHandler(writeToFile,file="build_and_save_HIO_models.log")
+addHandler(writeToFile,file="build_and_save_REF_and_HIO_models.log")
 
-modelmeta <- list("USEEIOv2.0.2-walrus"= c("USEEIOv2.0.2-walrus"),
-                  "USEEIO-WARM"= c("USEEIO-WARM", "WARMv15"),
+parentmodelmeta <- list("USEEIOv2.0.2-walrus"= c("USEEIOv2.0.2-walrus"),
+                        "USEEIO-REI-Reference" = c("USEEIO-REI-Reference","REI_WIO")
+                    )
+
+HIOmodelmeta <- list("USEEIO-WARM"= c("USEEIO-WARM", "WARMv15"),
                   "USEEIO-Waste-Disagg"= c("USEEIO-Waste-Disagg","LandfillingDisaggregation","562OTHDisaggregation",
                                           "WasteCombustorsDisaggregation","MRFsDisaggregation"),
                   "USEEIO-REI"= c("USEEIO-REI","REI_WIO"),
@@ -12,14 +15,31 @@ modelmeta <- list("USEEIOv2.0.2-walrus"= c("USEEIOv2.0.2-walrus"),
                                          "562OTHDisaggregation", "WasteCombustorsDisaggregation", "MRFsDisaggregation")
                   )
 
-for (name in names(modelmeta)) {
-  cpaths <- c()
-  for (cfg in modelmeta[[name]] ) {
-    cpaths <- append(cpaths,paste0(cfg,".yml"))
+saveModels <- function (v,meta) {
+   for (i in 1:length(v)) {
+    name <- names(meta)[i]
+    cpaths <- c()
+    for (cfg in meta[i]) {
+      cpaths <- append(cpaths,paste0(cfg,".yml"))
+    }
+    loginfo(paste("Attempting to build",name))
+    model <- buildModel(name, configpaths=file.path(cpaths))
+    savename <- paste0(prefix,v[i])
+    saveRDS(model,paste0(savename,".rds"))  
+    loginfo(paste("Saved",name,"as",savename,"to .rds"))
   }
-  loginfo(paste("Attempting to build",name))
-  model <- buildModel(name, configpaths=file.path(cpaths))
-  saveRDS(model,paste0(name,".rds"))  
-  loginfo(paste("Saved",name,"to .rds"))
-}
+} 
+
+v <- c("USEEIO","REI")
+prefix <- "REF-"
+saveModels(v,parentmodelmeta)
+
+
+v <- c(1:4)
+prefix <- "HIO-"
+saveModels(v,HIOmodelmeta)
+
+
+
+ 
 

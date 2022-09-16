Fix units issue in REI-REF model

scripts/parse_REI_env_data.py
@@ -46,15 +46,15 @@ col_order = ['Sector',
 
 df = df[col_order]
 
+df['FlowAmount'] = np.where(df['Unit'] == '1000 USD',
+                            df['FlowAmount'] * 1000,
+                            df['FlowAmount'])
+df['Unit'] = np.where(df['Unit'] == '1000 USD',
+                      'USD', df['Unit'])
+
 df.to_csv(f'{out_path}/REI_primaryfactors_TBS.csv', index=False)
 
 df1 = df.query('Sector.str.startswith("RS")', engine='python')
-
-df1['FlowAmount'] = np.where(df1['Unit'] == '1000 USD',
-                             df1['FlowAmount'] * 1000,
-                             df1['FlowAmount'])
-df1['Unit'] = np.where(df1['Unit'] == '1000 USD',
-                       'USD', df1['Unit'])
 
 # Map REI flows to FEDEFL flows
 mapping = {"Wage":"Compensation of employees",
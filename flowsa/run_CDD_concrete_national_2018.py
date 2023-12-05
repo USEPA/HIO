97dbedbb527beb7a0bc89df4a7b14f81b1a2584d
WesIngwersen <ingwersen.wesley@epa.gov>
2023-12-04 19:19:10 -0500

    update code for CDD_concrete fbs to run 2018 file..fix paths in 2018 yaml includes

--- flowsa/run_CDD_concrete_national_2018.py
+++ flowsa/run_CDD_concrete_national_2018.py
@@ -1,7 +1,7 @@
 """
-Generates CDD concrete national 2014 flow-by-sector model and saves it to local cache
+Generates CDD concrete national 2018 flow-by-sector model and saves it to local cache
 along with the validation log
-See the notes on 'CDD_concrete_national_2014.yaml' for requirements
+See the notes on 'CDD_concrete_national_2018.yaml' for requirements
 """
 
 import os
@@ -12,8 +12,7 @@ DIRPATH = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/') + '/'
 fbsMethodPath = f"{DIRPATH}"
 
 # Run an FBS method not included in the flowsa repo
-method = 'CDD_concrete_national_2014'
+method = 'CDD_concrete_national_2018'
 flowsa.flowbysector.main(method=method, fbsconfigpath=fbsMethodPath,
                          download_FBAs_if_missing=True)
-fbs = flowsa.getFlowBySector(method)
 
